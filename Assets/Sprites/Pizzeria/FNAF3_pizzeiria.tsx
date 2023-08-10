e2ab1ae1981a0f42f92018b9ec69a0569d2c2728
Miłosz <milosz.plask@gmail.com>
2023-08-10 22:45:20 +0200

    added final version of pizzeria tilemap

---
+++ Assets/Sprites/Pizzeria/FNAF3_pizzeiria.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="FNAF3_pizzeiria" tilewidth="32" tileheight="32" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Images/pizzeria1.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 0,32 -32,32 -32,16 -16,16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Images/pizzeria2.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 0,32 32,32 32,16 16,16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Images/pizzeria3.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="32">
+    <polygon points="0,0 0,-32 32,-32 32,-16 16,-16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Images/pizzeria4.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="32">
+    <polygon points="0,0 0,-32 -32,-32 -32,-16 -16,-16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Images/pizzeria5.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 0,15.875 -15.875,16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Images/pizzeria6.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="32.125" y="16.125">
+    <polygon points="0,0 0,15.875 -15.875,16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="Images/pizzeria7.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.125" y="16">
+    <polygon points="0,0 0,15.875 -15.875,16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Images/pizzeria8.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.875" y="0.125">
+    <polygon points="0,0 0,15.875 -15.875,16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Images/pizzeria9.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="Images/pizzeria11.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="32">
+    <polygon points="0,0 32,0 32,-16.125 -0.125,-16.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="Images/pizzeria12.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.375" y="16.25">
+    <polygon points="0,0 32,0 32,-16.125 -0.125,-16.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="Images/pizzeria13.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="32" y="0">
+    <polygon points="0,0 0,32 -16.125,32.125 -16.125,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="Images/pizzeria14.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="16" y="0.125">
+    <polygon points="0,0 0,32 -16.125,32.125 -16.125,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
