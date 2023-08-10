c47d60a16d4dcb098b2c5b9603885c8d4c9ecd13
Miłosz <milosz.plask@gmail.com>
2023-08-10 22:10:12 +0200

    adds SuperTiled2Unity extenstion

---
+++ Assets/Sprites/Pizzeria/FNAF3.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="FNAF3" tilewidth="32" tileheight="32" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="pizzeria3.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="pizzeria4.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="pizzeria5.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="pizzeria6.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="pizzeria7.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="pizzeria8.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="pizzeria9.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="pizzeria10.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="pizzeria1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="pizzeria2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="pizzeria11.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="pizzeria12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="pizzeria14.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="pizzeria13.png"/>
+ </tile>
+</tileset>
