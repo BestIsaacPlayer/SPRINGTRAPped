696c3edc91b5ed46624cb3e241c7e5037f22f379
Miłosz <milosz.plask@gmail.com>
2023-08-10 02:34:25 +0200

    adjusted animation speed

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public float moveSpeed = 5f;
+    
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    private Animator _animator;
+
+    private void Start()
     {
-        
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
         
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _movement.normalized * (moveSpeed * Time.fixedDeltaTime));
     }
 }
